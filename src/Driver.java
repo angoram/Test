4788a5b8a5e2884efbc479b5ba9ef8cbcb854f32
angoram <kor.andrew@outlook.com>
2020-02-07 23:15:13 -0500

    added interface and appropriate methods

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,12 @@
-public class Driver {
-    public static void main(String[] args){
+public class Driver implements SampleInterface {
+
+    public static void main(String[] args) {
         System.out.println("Hello world from desktop.");
+        SampleInterface.method();
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("Overridden method");
     }
 }
---
+++ src/SampleInterface.java
@@ -0,0 +1,6 @@
+public interface SampleInterface {
+    static void method(){
+        System.out.println("Hello world");
+    }
+    void method2();
+}
